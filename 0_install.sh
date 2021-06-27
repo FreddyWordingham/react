262a596741abac0691ee84e1b6e64fe65e45ec84
Freddy Wordingham <f.wordingham@exeter.ac.uk>
2021-06-27 18:47:54 +0100

    Added frontend.

--- 0_install.sh
+++ 0_install.sh
@@ -1,3 +1,6 @@
+sudo apt-get -y install python3;
+sudo apt-get -y install python3-pip;
+sudo apt-get -y install node;
 pip3 install django;
 pip3 install djangorestframework;
 pip3 install django-cors-headers;
